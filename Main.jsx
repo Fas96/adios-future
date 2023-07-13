746a00f9285f7cab66a944e982099cd3227dc7ab
fasbhim <https://gitlab.argonet.co.kr/Fas96>
2023-07-13 20:24:16 +0900

    "adios"

---
+++ Main.jsx
@@ -0,0 +1,18 @@
+import {View, Text, SafeAreaView, StatusBar, Platform} from 'react-native'
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import Login from './screens/Login';
+
+const Stack = createNativeStackNavigator();
+const Main = () => {
+    return (<NavigationContainer >
+        <Stack.Navigator>
+            <Stack.Group>
+                <Stack.Screen name="home" component={Home}/>
+            </Stack.Group>
+        </Stack.Navigator>
+    </NavigationContainer>)
+}
+export default Main
